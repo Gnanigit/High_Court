ceadc71a365887db28d58eaea2dc7661c8f437ad
dev1_clofast <gnani4412@gmail.com>
2025-04-03 09:10:56 +0530

    added database to store the file

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes/file.js";
+import path from "path";
 
 // Load environment variables
 dotenv.config();
